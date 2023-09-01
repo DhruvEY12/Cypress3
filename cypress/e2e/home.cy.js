ad157b5337cbf023a763bf3d4a92369dd280c38e
DhruvEY12 <138430745+DhruvEY12@users.noreply.github.com>
2023-09-01 10:44:45 +0530

    Update home.cy.js

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,29 +1,130 @@
-describe('Test Fire', () => {
-  beforeEach(() => {
-    cy.visit('https://demo.testfire.net/')
-  })
-  it('Goes to body and search items', () => {
-    cy.get('body')
-    cy.get('#LinkHeader2').contains('PERSONAL')
-    cy.get('#MenuHyperLink1').contains('Deposit Product')
-  })
+// Login "SDMSUAT"
 
-  it('Entering data into Search bar', () => {
-    cy.get('#query').type('testing for devsecops')
-    cy.contains('Go').click()
-  })
+describe('SDMS Login Test', () => {
+  it('Login', () => {
 
-  it('Entering data into Feedback', () => {
-    cy.get('#HyperLink4').click()
-    cy.contains('Your Name:')
-    cy.get(':nth-child(2) > [valign="top"] > input').type('Anmol')
-    cy.contains('Your Email Address:')
-    cy.get(':nth-child(3) > [valign="top"] > input').type('anmol@gmail.com')
-    cy.contains('Subject:')
-    cy.get(':nth-child(4) > [valign="top"] > input').type('filling the form')
-    cy.get('textarea').type('I m filling this form via cypress test script to check if we are able to fill it from here or not')
-    cy.contains('Submit').click()
+    cy.visit('https://sdmsuat.dalmiabharat.com/login')
 
-  })
+    cy.get(':nth-child(3) > .form-control').type('v-ankur.aggarwal')
 
-})
+    cy.get(':nth-child(4) > .form-control').type('dalmia@1234')
+
+    cy.get('.btn').click()
+
+    //create a new sceheme
+
+  cy.get('[title="Add New scheme"] > .fa').click()
+
+    
+  cy.get('select[name="comcodefrom"]') 
+    .select("1000 - Dalmia Cement(Bharat) Ltd")
+    .should("have.value", "1000");
+
+  cy.get('select[name="custschemeType"]') 
+   .select("Annual Discount")
+   .should("have.value", "RBAD");
+
+  cy.get(':nth-child(5) > .col-md-5 > .row > :nth-child(2) > .form-control').type('DevSecOps_Scheme')
+
+  cy.get('input[name="scheme.fromDate"]') 
+  .clear() 
+  .type("2023-08-01");
+
+  cy.get('input[name="scheme.toDate"]')
+  .clear() 
+  .type("2023-08-10");
+
+  cy.get('input[name="scheme.announcementDate"]')
+  .clear()
+  .type("2023-07-30");
+
+  cy.get('#accordion1 > .card > .card-header > .card-link > b').click()
+  
+  cy.get('select[name="sorgFrom"]')
+  .select("1000") 
+  .should("have.value", "1000"); 
+
+
+  cy.get('select[name="dcFrom"]')
+  .select("01") 
+  .should("have.value", "01"); 
+
+
+  cy.get('select[name="div1"]') 
+  .select("01") 
+  .should("have.value", "01"); 
+
+  cy.get('#accordion1 > .card > .card-header > .card-link > b').click()
+
+  cy.get('#accordion2 > .card > .card-header > .card-link > b').click()
+
+  cy.get('select[name="cr1"]') 
+  .select("01")
+  .should("have.value", "01");
+
+  cy.get('#accordion2 > .card > .card-header > .card-link > b').click()
+
+  cy.get('#accordion4 > .card > .card-header > .card-link > b').click()
+
+  cy.wait(200);
+  cy.get('select[name="scheme.glAccountItem"]')
+  .select("0014200260")
+  .should("have.value", "0014200260");
+
+  cy.get('#accordion4 > .card > .card-header > .card-link > b').click()
+
+  cy.get('input[type=file]').selectFile("cypress/fixtures/Question 15.pdf", { force: true })
+  
+  cy.get('input[name="slabCat"][value="slab"]').check();
+
+  cy.contains('Upload Document').click({ force: true });
+
+  cy.get('input[name="slabCat"][value="slab"]').check();
+
+  cy.get('select[name="schemeType1"]') 
+   .select("Total Actual Qty Slab_Divyansh")
+   .should("have.value", "1486");
+
+cy.get('td input').eq(0).type('10'); // Set "10" in the first input
+cy.get('td input').eq(1).type('20'); // Set "20" in the second input
+cy.get('td input').eq(2).type('30'); // Set "30" in the third input
+cy.get('td input').eq(3).type('40'); // Set "40" in the fourth input
+
+cy.contains('View Scheme').click();
+
+cy.get('button.btn.btn-primary.view[type="button"]').click();
+
+//Filter scheme
+
+cy.get('button.button-class.sm').first().click(); 
+cy.wait(1000);
+cy.contains('b', 'PD Portal').click();
+cy.get('a.side-nav[href="/pdportalpost"]').click({ multiple: true }); 
+cy.get('div.col-md-12 button[data-toggle="tooltip"][title="Discount Calculation"]:contains("Filters")').click({ force: true });
+cy.get('select[name="filterStatus"]').select('Partially Posted');
+cy.contains('Get Data').click();
+
+// //Batch Job
+
+cy.contains('Tools & Setup').click();
+cy.get('a.side-nav[href="/batch"]').click();
+cy.get('select[name="searchBy"]').select('Batch Name');
+cy.get('select[name="searchBy"]').select('Batch Name');
+cy.wait(1000);
+cy.get('select[name="fieldtext"]').select('BKPF');
+
+// //Analysis Report
+
+cy.get('button.button-class2').click();
+cy.get('a.side-nav[href="/analysisreport"]').click();
+cy.get('input[name="fromDate"]').type('2023-08-01');
+cy.get('input[name="toDate"]').type('2023-08-30');
+cy.get('select[name="regionId"]').select('2');
+cy.get('button.btn.btn-primary').click();
+
+
+
+
+});
+  
+});
